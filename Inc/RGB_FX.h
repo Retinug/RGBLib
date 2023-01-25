bb311af68a1b627cc6a3fb6a463f51dfe0a200bb
Retinug <Retinug@gmail.com>
2023-01-25 12:25:53 +0300

    Added color segmentation

--- Inc/RGB_FX.h
+++ Inc/RGB_FX.h
@@ -4,22 +4,35 @@
 #include "stm32f1xx_hal.h"
 
 #include "RGBLib.h"
+
+#define SEGMENT_SIZEMAX 5
+
+#define FX_MODE_OFF 0
+#define FX_MODE_STATIC 1
+
 typedef struct TYPE_FX_SEGMENT
 {
   uint16_t start;
   uint16_t end;
+  uint8_t mode; //mode effect
+  uint8_t options;
   uint8_t counterTarget;
   uint8_t counterCurrent;
+  uint8_t colorHue;
 } FX_Segment;
 
 typedef struct TYPE_FX_SEGMENT_MANAGER
 {
-  FX_Segment *segments[SEGMENT_SIZEMAX];
-  uint8_t segmentsSize;
+  FX_Segment segments[SEGMENT_SIZEMAX];
 } FX_Segment_Manager;
-void FX_ManagerUpdate(void);
 
-void FX_SetSegment(uint8_t num, uint16_t start, uint16_t end, uint8_t counter_target);
+
+void FX_Update(void);
+
+void FX_SetSegment(uint8_t num, uint16_t start, uint16_t end, uint8_t mode, uint8_t counterTarget, uint8_t hue);
+void FX_SegmentStep(FX_Segment *segment);
 void FX_SegmentUpdate(FX_Segment *segment);
 
+void FX_Off(FX_Segment *segment);
+void FX_Static(FX_Segment *segment);
 #endif
--- Src/RGB_FX.c
+++ Src/RGB_FX.c
@@ -4,27 +4,70 @@ FX_Segment_Manager SegmentManager;
 
 void FX_Update(void)
 {
-  for (int i = 0; i < SegmentManager.segmentsSize; i++)
+  for (int i = 0; i <= SEGMENT_SIZEMAX - 1; i++)
   {
-    FX_SegmentUpdate(SegmentManager.segments[i]);
+    FX_SegmentStep(&SegmentManager.segments[i]);
   }
+  RGB_Send();
 }
 
-void FX_SetSegment(uint8_t num, uint16_t start, uint16_t end, uint8_t counterTarget)
+void FX_SetSegment(uint8_t num, uint16_t start, uint16_t end, uint8_t mode, uint8_t counterTarget, uint8_t hue)
 {
-  FX_Segment *Segment;
-  Segment = SegmentManager.segments[num];
-  Segment->start = start;
-  Segment->end = end;
-  Segment->counterTarget = counterTarget;
+  //FX_Segment Segment;
+  //Segment = &SegmentManager->segments[num];
+  SegmentManager.segments[num].start = start;
+  SegmentManager.segments[num].end = end;
+  SegmentManager.segments[num].mode = mode;
+  SegmentManager.segments[num].counterTarget = counterTarget;
+  SegmentManager.segments[num].colorHue = hue;
+  //SegmentManager->segments[num] = Segment;
+  //Segment->FX_Effect = FX_Effect;
 }
 
-void FX_SegmentUpdate(FX_Segment *segment)
+void FX_SegmentStep(FX_Segment *segment)
 {
+  if(segment->counterTarget == 0)
+  {
+    return;
+  }
   segment->counterCurrent++;
   if(segment->counterCurrent >= segment->counterTarget)
   {
     segment->counterCurrent = 0;
-    //update effect
+    FX_SegmentUpdate(segment);
+  }
+}
+
+void FX_SegmentUpdate(FX_Segment *segment)
+{
+  switch (segment->mode) {
+    case FX_MODE_OFF:
+      FX_Off(segment);
+      break;
+    case FX_MODE_STATIC:
+      FX_Static(segment);
+      break;
+    default:
+      break;
+  }
+}
+
+void FX_Off(FX_Segment *segment)
+{
+  for (uint16_t i = segment->start; i < segment->end; i++)
+  {
+    RGB_FillPixelRGB(i, 0, 0, 0);
+  }
+}
+
+void FX_Static(FX_Segment *segment)
+{
+  uint8_t r, g, b;
+  RGB_HSVtoRGB(segment->colorHue, 255, 255, &r, &g, &b);
+  for (uint16_t i = segment->start; i <= segment->end; i++)
+  {
+    RGB_FillPixelRGB(i, r, g, b);
+  }
+}
   }
 }
